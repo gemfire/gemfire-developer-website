ff491b8b1099457cbe85366c746ef21fb71afdae
Owen Nichols <onichols@apache.org>
2024-03-07 13:52:08 -0800

    add 9.15.11, 10.0.3, and 10.1.0 javadocs to static as well as Search 1.0.0

---
+++ static/api/extensions/search/1.0/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.apache.geode.cache.lucene"},{"l":"org.apache.geode.cache.lucene.management"},{"l":"org.apache.geode.cache.lucene.management.configuration"}]
